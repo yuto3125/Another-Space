bb726b6ccb9f22adaa5aecc9ab9fd623d5af0365
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2020-11-03 09:31:45 +0900

    Update index.js

--- router-tutorial/src/index.js
+++ router-tutorial/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'C:/Users/kuuha/AppData/Roaming/npm/node_modules/react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
